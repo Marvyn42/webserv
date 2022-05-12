ca1c0b6c5889287dee61ee74d3448b5073c65f1c
Marvyn MAQUIGNON <mamaquig@e1r8p1.clusters.42paris.fr>
2022-05-12 15:06:32 +0200

    In progress

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 - TCP Server side
     - socket
     - bind
+	poll() {
     - listen
     - accept
     - send
     - recv
+	}
 
 	struct  pollfd
 	{
@@ -16,6 +18,8 @@
 */
 
 #include <cstdlib>
+#include <vector>
+#include <unistd.h>
 #include <cstring>
 #include <iostream>
 #include <sys/socket.h>
@@ -28,18 +32,17 @@
 #define SEND_ERROR -1
 #define RECV_ERROR -1
 #define POLL_ERROR -1
+#define BUFF 10
 
 int	main(void) {
-	//stocker dans struct ? class ?
 	struct sockaddr_in	address;
 	int					server_fd;
 	int					new_socket;
 	socklen_t			addrlen = sizeof(address);
 	const int			PORT = 8080;
-	struct pollfd		fd[1];
-	//
-	//parsing?
+	std::vector<struct pollfd> fds;
 
+	/*=======================================SET SERVER=====================================================*/
 	if (server_fd = socket(PF_INET, SOCK_STREAM, 0) == INVALID_SOCKET) {
 		std::cerr << "error: socket creation" << std::endl;
 		return(EXIT_FAILURE);
@@ -52,37 +55,44 @@ int	main(void) {
 		std::cerr << "error: socket bind" << std::endl;
 		return(EXIT_FAILURE);
 	}
-	//debut boucle pool ? pool simple ? POLLNVAL
-	if (poll(fd, 1,  100) == POLL_ERROR)
-		// ???
-	if (listen(server_fd, 2) == -1) {
-		std::cerr << "error: socket listen" << std::endl;
-		exit(EXIT_FAILURE);
+	struct pollfd fd;
+	fd.fd = server_fd;
+	fd.events = POLLIN;
+	fds.push_back(fd);
+	/*=======================================SERVER ROUTINE=====================================================*/
+	while (1) {
+		if (poll(&fds[0], fds.size(), 0) != POLL_ERROR) {
+			/*==========partie connection client==========*/
+			if (fds[0].revents == POLLIN ) {
+				//listen puis accept le nouveau client si POLLIN
+				if (listen(server_fd, BUFF) == -1) { //ECONNREFUSED
+					std::cerr << "error: socket listen" << std::endl;
+					exit(EXIT_FAILURE);
+				}
+				if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) == INVALID_SOCKET) {
+					std::cerr << "error: socket accept" << std::endl;
+					exit(EXIT_FAILURE);
+				}
+				else {
+					//add_client
+				}
+			}
+			/*==========partie requete client==========*/
+				//iter sur la liste des client pour une action en fonction des signaux sortant:
+					//client recois
+					//client envoi
+					//client se deco
+			// if(send(new_socket, buffer, strlen(buffer), 0) == SEND_ERROR) {
+				// std::cerr << "error: send data" << std::endl;
+				// exit(EXIT_FAILURE);
+			// }
+			// if((n = recv(new_socket, buffer, sizeof buffer - 1, 0)) == RECV_ERROR) {
+				// std::cerr << "error: receive data" << std::endl;
+				// exit(EXIT_FAILURE);
+			// }
+		}
+		usleep(100);
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) == INVALID_SOCKET) {
-		std::cerr << "error: socket accept" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	//-------------------------------
-	//	Switch en fonction du signal ?
-	switch(poll(fd, 1,  100)) {
-		case POLL_ERROR:
-			std::cerr << "error: poll" << std::endl;
-			return(EXIT_FAILURE);
-		case 0:
-	}
-	//-------------------------------
-	if (poll( struct pollfd fds[],  nfds_t nfds,  int n_msec ))
-		//
-	//-------------------------------
-	if(send(new_socket, buffer, strlen(buffer), 0) == SEND_ERROR) {
-		std::cerr << "error: send data" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	if((n = recv(new_socket, buffer, sizeof buffer - 1, 0)) == RECV_ERROR) {
-		std::cerr << "error: receive data" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	buffer[n] = '\0';
+	/*============================================================================================*/
 	return(EXIT_SUCCESS);
 }
